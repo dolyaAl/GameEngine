c36ec1eb676fbeffcfa86b78de125d55b50eaf8c
DolyaAl <lyosha.dolya@gmail.com>
2021-02-08 15:14:26 +0300

    Added loadTexture method

--- src/Resources/ResourceManager.cpp
+++ src/Resources/ResourceManager.cpp
@@ -5,6 +5,11 @@
 #include<fstream>
 #include<iostream>
 
+#define STB_IMAGE_IMPLEMENTATION
+#define STB_ONLY_PNG
+#include "stb_image.h"
+
+
 ResourceManager::ResourceManager(const std::string& executablePath)
 {
     size_t found = executablePath.find_last_of("/\\");
@@ -46,6 +51,24 @@ std::shared_ptr<Renderer::ShaderProgram> ResourceManager::getShaderProgram(const
     }
     std::cout << "Can't find shader program: " << shaderName << std::endl;
     return nullptr;
+}
+
+void ResourceManager::loadTexture(const std::string& textureName, const std::string& texturePath)
+{
+    int channels = 0;
+    int width = 0;
+    int height = 0;
+
+    stbi_set_flip_vertically_on_load(true);
+    unsigned char* pixels = stbi_load(std::string(m_path + "/" + texturePath).c_str(), &width, &height, &channels, 0);
+
+    if (!pixels)
+    {
+        std::cout << "Can't load image: " << texturePath << std::endl;
+        return;
+    }
+
+    stbi_image_free(pixels);
 }
 
 std::string ResourceManager::getFileString(const std::string& relativeFilePath) const
